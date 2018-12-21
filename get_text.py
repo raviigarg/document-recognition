a9689deaa8342261ecd71ac23e6d42e5b3c131ee
Ravi Kant Garg <ravii.garg24@gmail.com>
2018-12-21 22:11:02 +0530

    Text extracted from remote image and pdf

--- get_text.py
+++ get_text.py
@@ -29,12 +29,41 @@ def ocr_space_file(filename, overlay=False, api_key='helloworld', language='eng'
                           )
         return r.content.decode()
 
+def ocr_space_url(url, overlay=False, api_key='helloworld', language='eng'):
+    """ OCR.space API request with remote file.
+        Python 3.5 - not tested on 2.7
+    :param url: Image url.
+    :param overlay: Is OCR.space overlay required in responses.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                     List of available languages codes can be found on https://ocr.space/OCRAPI
+                     Defaults to 'en'.
+    :return: Result in json format.
+    """
+
+    payload = {'url': url,
+               'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+              }
+    r = requests.post('https://api.ocr.space/parse/image',
+                      data=payload,
+                     )
+    return r.content.decode()
+
 def main():
     test_file_local = ocr_space_file(filename='data/pdfs/sample-pdf.pdf', language='eng')
+    test_file_remote = ocr_space_url('file_url', language='eng') # replace value of file_url with valid url of image or pdf
     json_string_local = json.loads(test_file_local)
     extracted_text_local = json_string_local["ParsedResults"][0]["ParsedText"]
     extracted_text_local = extracted_text_local.replace(" \r\n", " ")
+    json_string_remote = json.loads(test_file_remote)
+    extracted_text_remote = json_string_remote["ParsedResults"][0]["ParsedText"]
+    extracted_text_remote = extracted_text_remote.replace(" \r\n", " ")
     print("Local file data : " + extracted_text_local)
+    print("Remote file data : " + extracted_text_remote)
    
     
 if __name__ == "__main__":
