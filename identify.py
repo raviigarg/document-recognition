1748c8f25f3102e4b088da9c619d520cba1d766f
Ravi Kant Garg <ravii.garg24@gmail.com>
2018-12-26 14:54:45 +0530

    Made predictions

--- identify.py
+++ identify.py
@@ -2,9 +2,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
-
-# import dataset into pandas dataframe using the read_table method
+# import training dataset into pandas dataframe using the read_table method
 """
 dataset is tab separated.
 we will be using '\t' as the value for 'sep' argument which specify this format.
@@ -14,7 +14,7 @@ df = pd.read_table("doccollection/DocCollection", sep='\t', header=None,
                    names=['label', 'content'])
 
 # Data Preprocessing
-# map labels yes to 1 and no to 0
+# map labels yes to 1 and no to 0 for training dataset
 df['label'] = df.label.map({'no':0, 'yes':1})
 
 # prepare training data
@@ -30,6 +30,26 @@ training_data = count_vector.fit_transform(x_train)
 naive_bayes = MultinomialNB()
 naive_bayes.fit(training_data, y_train)
 print("Data preprocessing done")
+
+# import testing dataset into pandas dataframe using the read_table method
+df_test = pd.read_table("doccollection/TestCollection", sep='\t', header=None, 
+                   names=['label', 'content'])
+
+# map labels yes to 1 and no to 0 for test dataset
+df_test['label'] = df_test.label.map({'no':0, 'yes':1})
+
+# prepare testing data
+x_test = df_test['content']
+y_test = df_test['label']
+
+# applying Bag of Words processing to our testing dataset using count vectorizer
+testing_data = count_vector.transform(x_test)
+
+# make predictions on testing dataset using predict method
+predictions = naive_bayes.predict(testing_data)
+
+# check accuracy
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
 
 
 
